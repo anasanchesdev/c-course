de59b7945370db46eaa8f4ad79ee06883ecdab68
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-09 15:29:22 -0300

    Inital commit

---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // constante: variável que não pode ser alterada pelo programa durante a execução
+    const float PI = 3.14159;
+
+    printf("%f", PI);
+
+    return 0;
+}
