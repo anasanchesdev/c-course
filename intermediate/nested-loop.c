95dab17e4b76fecd014c524d86254e6a874357fa
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-02 09:16:16 -0300

    Initial commit

---
+++ intermediate/nested-loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// loop dentro de loop
+
+int main()
+{
+    int rows;
+    int columns;
+    char symbol;
+
+    printf("Enter the number of rows:\n > ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns:\n > ");
+    scanf("%d", &columns);
+    printf("Enter a symbol:\n > ");
+    scanf("%c", &symbol);
+
+
+    return 0;
+}
