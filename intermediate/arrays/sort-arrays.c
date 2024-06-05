cf49fc9288f8b18e149bbd5b9d5ffbc14cbc8940
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-05 10:50:16 -0300

    comments

--- intermediate/arrays/sort-arrays.c
+++ intermediate/arrays/sort-arrays.c
@@ -7,7 +7,7 @@ void printArray(int[], int);
 int main()
 {
 
-    int array[] = {3, 2, 6, 7, 9, 1};
+    int array[] = {3, 2, 6, 7, 9, 1}; // é possível usar arrays de caracteres também
     int len = sizeof(array) / sizeof(array[0]);
 
     sort(array, len);
@@ -15,7 +15,7 @@ int main()
 
     return 0;
 }
-
+// len = 6 (len - 1 = 5 -> índices)
 void sort(int array[], int len)
 {
     for (int i = 0; i < len - 1; i++)
@@ -23,10 +23,14 @@ void sort(int array[], int len)
         for (int j = 0; j < len - 1; j++)
         {   
 
-            // se o elemento atual é maior que o próximo, trocam de posição até que eventualmente o maior elemento seja o último da lista
+            // se o elemento atual j é maior que o próximo, trocam de posição até que eventualmente o maior elemento seja o último da lista
+            // ao trocar > por <, a lista é ordenada de maneira decrescente -> os menores números passarão a ser jogados para a direita
             if (array[j] > array[j + 1])
             {
-                int temp_element = array[j];
+                // j é guardado em uma variável temporária
+                // j assume o valor do próximo elemento
+                // o próximo elemento troca de lugar com j
+                int temp_element = array[j]; 
                 array[j] = array[j + 1];
                 array[j + 1] = temp_element;
             }
