b41672fc2ebb4117e80d6057ccb34fb8e6479851
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-11 12:38:22 -0300

    added questions and initial structure

--- small-projects/quiz-game/quiz-game.c
+++ small-projects/quiz-game/quiz-game.c
@@ -3,7 +3,33 @@
 
 int main()
 {
+    char guess;
+    int right_guesses;
+    int i = 0;
 
+    char questions[][100] = 
+    {
+        "1. What does \"int\" define?:\n > ",
+        "2. What is the function to display values on the terminal?",
+        "3. Loop structure?"
+        ""
+    };
+    char options[][100] = 
+    {
+        "A. Integer value\nB. Float number",
+        "A. printf()\nB. scanf()",
+        "A. switch()\nB. for()"
+    };
+    char answers[][6] = {"A", "A", "B"};
+
+    int num_of_questions = sizeof(questions)/sizeof(questions[0]);
+    
+    while (i < num_of_questions)
+    {
+        printf("%s\n%s", questions[0], options[0]);
+        printf("\n > ");
+        scanf("%c", &guess);
+    }
 
     return 0;
 }
